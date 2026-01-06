2b35d3ad03a0ddd5fcbcb801fdee107d6c56b49b
Zahralsayed <zalmatooq31@gmail.com>
2026-01-06 19:38:15 +0300

    update orders model

---
+++ src/main/java/com/Bookstore/model/Orders.java
@@ -0,0 +1,49 @@
+package com.Bookstore.model;
+
+import com.Bookstore.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
+@Entity
+@Table(name = "order")
+public class Orders {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @OneToMany(
+            mappedBy = "order",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+
+    @Column
+    @CreationTimestamp
+    private LocalDateTime orderDate;
+
+    @Column
+    private double totalPrice;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+}
