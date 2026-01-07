d3f0f6a184a1a9a58a164dc5afeab937629a30a2
Zahralsayed <zalmatooq31@gmail.com>
2026-01-08 01:55:37 +0300

    Orders Controller [API's]

---
+++ src/main/java/com/Bookstore/controller/OrdersController.java
@@ -0,0 +1,57 @@
+package com.Bookstore.controller;
+
+import com.Bookstore.enums.OrderStatus;
+import com.Bookstore.model.Orders;
+import com.Bookstore.model.User;
+import com.Bookstore.service.OrdersService;
+import com.Bookstore.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrdersController {
+    private OrdersService ordersService;
+
+    @Autowired
+    public void setOrdersService(OrdersService ordersService) {
+        this.ordersService = ordersService;
+    }
+
+    @PostMapping("/new")
+    public Orders createOrder(@RequestBody Orders order) {
+        User currentUser = OrdersService.getCurrentLoggedInUser();
+        return ordersService.createOrder(order, currentUser);
+    }
+
+    @GetMapping
+    public List<Orders> getMyOrders() {
+        User currentUser = OrdersService.getCurrentLoggedInUser();
+        return ordersService.findUserOrders(currentUser);
+    }
+
+    @GetMapping("/{id}")
+    public Orders getOrderById(@PathVariable long id) {
+        return ordersService.getOrderById(id);
+    }
+
+    @PutMapping("/{id}/cancel")
+    public Orders cancelOrder(@PathVariable long id) {
+        return ordersService.cancelOrder(id);
+    }
+
+    @PutMapping("/{id}/status")
+    public Orders updateOrderStatus(
+            @PathVariable long id,
+            @RequestParam OrderStatus status
+    ) {
+        return ordersService.updateOrderStatus(id, status);
+    }
+
+
+
+
+}
+
