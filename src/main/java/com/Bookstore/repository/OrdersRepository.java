da82f5635982705b1e0e476501abf26962d741df
Zahralsayed <zalmatooq31@gmail.com>
2026-01-08 00:14:24 +0300

    Order Repository

---
+++ src/main/java/com/Bookstore/repository/OrdersRepository.java
@@ -0,0 +1,10 @@
+package com.Bookstore.repository;
+
+import com.Bookstore.model.Orders;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrdersRepository extends JpaRepository<Orders, Long> {
+    List<Orders> findByUserId(Long userId);
+}
