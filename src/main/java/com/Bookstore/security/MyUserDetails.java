2b6ef9799e7dd9161b0aa86430602d9f8e3f73aa
algas <algassabk@gmail.com>
2026-01-12 18:09:45 +0300

    role fix

--- src/main/java/com/Bookstore/security/MyUserDetails.java
+++ src/main/java/com/Bookstore/security/MyUserDetails.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 
 public class MyUserDetails implements UserDetails {
@@ -19,6 +20,7 @@ public class MyUserDetails implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
 
     }
 
