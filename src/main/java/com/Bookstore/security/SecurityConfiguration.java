5ae8b608bc4731b277077006c41c57ee934f9172
zainabalzaimoor <alzaimoorzainab@gmail.com>
2026-01-20 23:06:17 +0300

    Added the seed file for testing

--- src/main/java/com/Bookstore/security/SecurityConfiguration.java
+++ src/main/java/com/Bookstore/security/SecurityConfiguration.java
@@ -48,8 +48,6 @@ public class SecurityConfiguration {
                         .requestMatchers("/auth/users/getAllUsers").hasRole("ADMIN")
                         .requestMatchers("/auth/users/status/{userId}").hasRole("ADMIN")
                         .requestMatchers("/auth/users/delete/{userId}").hasRole("ADMIN")
-//                        .requestMatchers("/api/categories", "/api/categories/*").authenticated()
-//                        .requestMatchers("/api/books/**").authenticated()
                         .anyRequest().authenticated()
 
                 )
